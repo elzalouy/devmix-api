8768c00e1ea92d0b4399e2a9e3acd8216141893c
Ezat Elzalouy <ezatelzalouy711@gmail.com>
2020-02-10 12:04:52 +0200

    image uploade

--- services/uploading.js
+++ services/uploading.js
@@ -1,7 +1,12 @@
 const multer = require("multer");
 const uuid4 = require("uuid/v4");
+const path = require("path");
+let dir = __dirname.split("/services");
 module.exports = function() {
   const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(dir[0], "/public/assets"));
+    },
     filename: (req, file, cb) => {
       const fileName = file.originalname
         .toLowerCase()
